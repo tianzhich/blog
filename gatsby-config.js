269972340341fb5d119b7504445d57f65756da00
tianzhich <zhi.tian@yahoo.com>
2018-08-17 02:05:47 +0800

    add KaTex support for math in markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,11 +13,18 @@ module.exports = {
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    'gatsby-transformer-remark',
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-katex`,
+        ],
       },
     }
   ],
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import React from "react"; 
 import styled from 'styled-components'
+import "katex/dist/katex.min.css"
 
 const PostWrapper = styled.div`
   width: 90%;
