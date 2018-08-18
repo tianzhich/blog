3b585e69874cf5766c94c23c5e9633b1da7170cd
tianzhich <zhi.tian@yahoo.com>
2018-08-18 16:16:42 +0800

    modify 404 page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 import Header from '../components/header'
-import './index.css'
+import './index.scss'
 import styled from 'styled-components'
 import heartLogo from '../images/valentines-heart.svg'
 
--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,8 @@ import React from 'react'
 const NotFoundPage = () => (
   <div>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <p>Please visit</p>
+    <p>There's not much left here for you ;)</p>
+    <p>这里啥也没有，请访问其他页面吧</p>
   </div>
 )
 
